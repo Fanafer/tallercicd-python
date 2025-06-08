33e8a9fe604df72ce9ff4e41876b5512a0085a15
fanafer <fackundofernandez@gmail.com>
2025-06-08 17:23:53 -0300

    agregué lógica del juego

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "¡Hola desde Flask con CI/CD!"
+@app.route("/", methods=["GET", "POST"])
+def jugar():
+    resultado = jugador = bot = None
+    opciones = ["piedra", "papel", "tijeras"]
+
+    if request.method == "POST":
+        jugador = request.form["choice"]
+        bot = random.choice(opciones)
+
+        if jugador == bot:
+            resultado = "Empate"
+        elif (jugador == "piedra" and bot == "tijeras") or \
+             (jugador == "papel" and bot == "piedra") or \
+             (jugador == "tijeras" and bot == "papel"):
+            resultado = "Ganaste!"
+        else:
+            resultado = "Perdiste"
+
+    return render_template("index.html", resultado=resultado, jugador=jugador, bot=bot)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
