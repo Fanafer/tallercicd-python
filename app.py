73491adb0dc505a9349670388341c7557e35d8e1
fanafer <fackundofernandez@gmail.com>
2025-06-08 23:42:46 -0300

    Añadí la función determinar_resultado

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ import random
 
 app = Flask(__name__)
 
+def determinar_resultado(jugador, bot):
+    if jugador == bot:
+        return "Empate"
+    elif (jugador == "piedra" and bot == "tijeras") or \
+         (jugador == "papel" and bot == "piedra") or \
+         (jugador == "tijeras" and bot == "papel"):
+        return "Ganaste!"
+    else:
+        return "Perdiste"
+
 @app.route("/", methods=["GET", "POST"])
 def jugar():
     resultado = jugador = bot = None
@@ -11,15 +21,7 @@ def jugar():
     if request.method == "POST":
         jugador = request.form["choice"]
         bot = random.choice(opciones)
-
-        if jugador == bot:
-            resultado = "Empate"
-        elif (jugador == "piedra" and bot == "tijeras") or \
-             (jugador == "papel" and bot == "piedra") or \
-             (jugador == "tijeras" and bot == "papel"):
-            resultado = "Ganaste!"
-        else:
-            resultado = "Perdiste"
+        resultado = determinar_resultado(jugador, bot)
 
     return render_template("index.html", resultado=resultado, jugador=jugador, bot=bot)
 
